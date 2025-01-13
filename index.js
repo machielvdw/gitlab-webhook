6363fe8c9b90ea359e6312c95bef4ce0f7b469dc
Machiel <machiel@lowdefy.com>
2025-01-13 14:13:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  const gitlabToken = req.headers['x-gitlab-token'];
+
+  // Respond immediately to avoid timeout
+  if (WEBHOOK_SECRET && gitlabToken !== WEBHOOK_SECRET) {
+    console.log('Secret token mismatch. Unauthorized request.');
+    return res.status(401).send('Unauthorized');
+  }
+
+  res.status(200).send('OK'); // Send response immediately
+
+  // Process payload after response
+  const gitlabEvent = req.headers['x-gitlab-event'];
+  const payload = req.body;
+
+  console.log('Received GitLab event:', gitlabEvent);
+  // console.log('Payload:', JSON.stringify(payload, null, 2));
+  console.log('Event data: ', payload.object_attributes);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Webhook listener running on port ${PORT}`);
+});
